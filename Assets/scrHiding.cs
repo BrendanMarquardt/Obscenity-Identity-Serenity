64cc84f3a4cfe2a335ba14b9651d75fe47059d3e
BrendyBoi <btm299@nyu.edu>
2018-10-02 09:39:39 -0400

    Created character, can control with arrow keys, and collide with ground and wall.

---
+++ Assets/scrHiding.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scrHiding : MonoBehaviour {
+
+	//static bool hiding;
+	//static float Xposition;
+	//static float Yposition;
+	//static float Zposition;
+	private MeshRenderer meshRend;
+
+	// Use this for initialization
+	void Start () {
+		meshRend = GetComponent <MeshRenderer> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+		//transform.position.x = scrMovement.Xposition;
+
+		if (Input.GetKey (KeyCode.Space)) {
+			meshRend.enabled = false;
+			//transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
+		} else {
+			meshRend.enabled = true;
+		}
+	}
+}
